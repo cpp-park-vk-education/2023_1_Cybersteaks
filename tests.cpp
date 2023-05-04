dd8b54148ab8122398b58abc3a79c31025c5183f
TanyaEmka <89926609+TanyaEmka@users.noreply.github.com>
2023-05-04 19:38:23 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,8 +1,22 @@
 #include "web.cpp"
+#include "orm.cpp"
 #include <gtest/gtest.h>
 
 TEST(WebTest, Programmer) {
     ASSERT_EQ(programmer_result, filterconv.ConvertFiltersSet(filtersets[2]));
+}
+
+TEST(ORMTest, ModelTest) {
+    ASSERT_EQ("{login: user}", model.Serialize());
+    ASSERT_EQ("user", model.Deserialize());
+}
+
+TEST(ORMTest, FeedTest) {
+    ASSERT_EQ(models.size(), feed.GetFeed(user).size());
+}
+
+TEST(ORMTest, ModelFabricTest) {
+    ASSERT_EQ(model.Serialize(), fabric.CreateModel("model").Serialize());
 }
 
 int main(int argc, char **argv) {
