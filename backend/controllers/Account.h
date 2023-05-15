660b61690283514e02c87150c02b6067b00731fd
wonderf00l <maks22036@gmail.com>
2023-05-15 22:46:16 +0300

    added backend

--- backend/controllers/Account.h
+++ backend/controllers/Account.h
@@ -8,25 +8,27 @@ class Account : public drogon::HttpController<Account> {
    public:
     void signup(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
 
-    void login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback,
-               std::string &&user_id, const std::string &password);
+    void login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
 
-    void settings(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback,
-                  std::string user_id, const std::string &token);
-
+    void logout(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+    
+    void posts(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+    
+    void settings(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+    
     void resume(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback,
                 std::string user_id);
 
-    void groups(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback,
-                std::string user_id);
+    void groups(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
 
     METHOD_LIST_BEGIN
-    METHOD_ADD(Account::signup, "/signup", Post);
-    METHOD_ADD(Account::login, "/login?userId={1}&passwd={2}",
-               Post);  // url: /account/login
-    METHOD_ADD(Account::settings, "/{1}/settings/?token={2}", Post,
-               Get);  // /account/{account_id}/settings?token=tok
-    METHOD_ADD(Account::resume, "/{1}/resume", Get);
-    METHOD_ADD(Account::groups, "/{1}/groups", Get);
+    METHOD_ADD(Account::signup, "/signup", Post, "empty_body_filter", "signup_filter");
+    METHOD_ADD(Account::login, "/login", Post, "empty_body_filter", "login_filter");  // url: /account/login
+    METHOD_ADD(Account::logout, "/logout", Delete, "empty_body_filter", "auth_filter");
+    METHOD_ADD(Account::settings, "/settings", Post, Get, Patch, "empty_body_filter", "auth_filter");
+    METHOD_ADD(Account::posts, "/posts", Post, Get, Delete, "empty_body_filter", "auth_filter");
+    METHOD_ADD(Account::groups, "/groups", Get, Post, Delete, "empty_body_filter", 
+               "auth_filter");  // post: group_id - patch, delete; get - groups list; post - list of user_id
+    METHOD_ADD(Account::resume, "/{1}/resume", Get, "auth_filter");
     METHOD_LIST_END
 };
