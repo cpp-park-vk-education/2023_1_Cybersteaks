4b138de853fef353ffd64ffb9aeb9688a68cd1a1
Slllenderman <ander12334545@mail.ru>
2023-05-16 16:54:55 +0300

    hw4

--- backend/go.sh
+++
@@ -1,5 +0,0 @@
-# build all targets and start drogon web server
-# use 'make test' to start tests
-
-rm -rf build && mkdir build && cd build && cmake -D BUILD_TESTS=ON .. && make
-cd ../build && ./increa_proj
