3f5e88fb8e9143dab8abbaa73a262712267d4ce7
Vladislav Fomenko <vladislav.fomenko.2014@gmail.com>
2023-05-16 14:23:34 +0300

    ORM pre-release

--- models/ORM/include/ORM.hpp
+++ models/ORM/include/ORM.hpp
@@ -1,22 +1,30 @@
 #pragma once
+
 #include <string>
 #include <map>
 #include <vector>
+#include <mysql/include/mysqlx/xdevapi.h>
+
+const std::string pathToConfigFile = "../SQLConfig.txt";
+const std::string dataBaseName = "MockDB";
 
 class ORM
 {
 public:
-    static void* Select(std::string table, std::string filter = "");
-    static void Delete(std::string table, std::string filter = "");
-    static void Insert(std::string table, std::string object);
-    static void Update(std::string table, std::string object);
-    static void CreateTable(std::string table);
-    static void DropDatabase(std::string database);
+    static std::vector<mysqlx::Row> Select(std::string &table);
+    static std::vector<mysqlx::Row> ORM::Filter(std::string &table, std::string &parametr);
+    static std::vector<mysqlx::Row> ORM::Find(std::string &table, std::string &object_id);
+    static bool Delete(std::string &table, std::string &object_id);
+    static bool Insert(std::string &table, std::map<std::string, std::string> &object);
+    static bool Update(std::string &table, std::map<std::string, std::string> &object);
+    static bool CreateTable(std::string &table, std::vector<std::string> &columns);
+    static bool DeleteTable(std::string &table);
+    static bool DropDatabase(std::string &database);
 
 private:
-    static bool ConnectionDB(std::string connection);
+    static std::unique_ptr<mysqlx::Session> ConnectionDB();
+    ;
 };
-
 
 class ORMGenerator
 {
@@ -28,8 +36,7 @@ public:
     void FillModels();
 };
 
-
-template<typename Model>
+template <typename Model>
 class IModelManager
 {
 public:
@@ -45,21 +52,25 @@ public:
     virtual Model Deserialize(std::map<std::string, std::string> object) = 0;
 };
 
-struct Filter {
+struct Filter
+{
     std::string name;
     std::string type;
     std::string model_field;
-    Filter(const std::string& name, const std::string& type, const std::string& model_field) {
+    Filter(const std::string &name, const std::string &type, const std::string &model_field)
+    {
         this->name = name;
         this->type = type;
         this->model_field = model_field;
     }
 };
 
-struct FilterSet {
+struct FilterSet
+{
     std::vector<Filter> filters;
     std::string model;
-    FilterSet(const std::string& model, std::vector<Filter> filters) {
+    FilterSet(const std::string &model, std::vector<Filter> filters)
+    {
         this->filters = filters;
         this->model = model;
     }
--- models/ORM/src/ORM.cpp
+++ models/ORM/src/ORM.cpp
@@ -1,12 +1,12 @@
 #include "ORM.hpp"
 
-#include <iostream>
+#include <map>
 #include <string>
 #include <fstream>
+#include <iostream>
 #include <exception>
-
-const std::string DIRECTORY_PATH = "../data/request.txt";
-const std::string TEST_CONNECTION = "connectionMOCK";
+#include <boost/algorithm/string.hpp>
+#include <mysql/include/mysqlx/xdevapi.h>
 
 class Error : public std::exception
 {
@@ -26,67 +26,232 @@ private:
     std::string message_;
 };
 
-void ORM::Select(std::string table)
+std::vector<mysqlx::Row> ORM::Select(std::string &table)
 {
-    if (!ConnectionDB(TEST_CONNECTION))
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
         throw Error("Connection is failed.");
+    mysqlx::Schema db = (*connection).getSchema(dataBaseName);
+    mysqlx::Table dbTable = db.getTable(table);
+    mysqlx::RowResult result = dbTable.select("*").execute();
 
-    std::ofstream SQLRequest(DIRECTORY_PATH, std::ofstream::out | std::ofstream::trunc);
-    SQLRequest << ("SELECT *\nFROM " + table + ";");
-    SQLRequest.close();
+    return result.fetchAll();
 }
 
-void ORM::Filter(std::string table, std::string parameter)
+std::vector<mysqlx::Row> ORM::Filter(std::string &table, std::string &parameter)
 {
-    if (!ConnectionDB(TEST_CONNECTION))
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
         throw Error("Connection is failed.");
+    mysqlx::Schema db = (*connection).getSchema(dataBaseName);
+    mysqlx::Table dbTable = db.getTable(table);
+    mysqlx::RowResult result = dbTable.select("*").where(parameter).execute();
 
-    std::ofstream SQLRequest(DIRECTORY_PATH, std::ofstream::out | std::ofstream::trunc);
-    SQLRequest << ("SELECT *\nFROM " + table + " WHERE " + parameter + ";");
-    SQLRequest.close();
+    return result.fetchAll();
 }
 
-void ORM::Find(std::string table, std::string object_id)
+std::vector<mysqlx::Row> ORM::Find(std::string &table, std::string &object_id)
 {
-    if (!ConnectionDB(TEST_CONNECTION))
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
         throw Error("Connection is failed.");
 
-    std::ofstream SQLRequest(DIRECTORY_PATH, std::ofstream::out | std::ofstream::trunc);
-    SQLRequest << ("SELECT *\nFROM " + table + " WHERE object_id = " + object_id + ";");
-    SQLRequest.close();
+    mysqlx::Schema db = (*connection).getSchema(dataBaseName);
+    mysqlx::Table dbTable = db.getTable(table);
+    mysqlx::RowResult result = dbTable.select("*").where("ID = " + object_id).execute();
+
+    return result.fetchAll();
 }
 
-void ORM::Delete(std::string table, std::string object_id)
+bool ORM::Delete(std::string &table, std::string &object_id)
 {
-    if (!ConnectionDB(TEST_CONNECTION))
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
         throw Error("Connection is failed.");
 
-    std::ofstream SQLRequest(DIRECTORY_PATH, std::ofstream::out | std::ofstream::trunc);
-    SQLRequest << ("DELETE *\nFROM " + table + " WHERE object_id = " + object_id + ";");
-    SQLRequest.close();
+    mysqlx::Schema db = (*connection).getSchema(dataBaseName);
+    mysqlx::Table dbTable = db.getTable(table);
+
+    (*connection).startTransaction();
+    try
+    {
+        dbTable.remove().where("ID = " + object_id).execute();
+        return true;
+    }
+    catch (const mysqlx::Error &err)
+    {
+        (*connection).rollback();
+    }
+
+    return false;
 }
 
-void ORM::Insert(std::string table, std::string object)
+bool ORM::Insert(std::string &table, std::map<std::string, std::string> &object)
 {
-    if (!ConnectionDB(TEST_CONNECTION))
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
         throw Error("Connection is failed.");
 
-    std::ofstream SQLRequest(DIRECTORY_PATH, std::ofstream::out | std::ofstream::trunc);
-    SQLRequest << ("INSERT INTO " + table + "\nVALUES " + object + ";");
-    SQLRequest.close();
+    mysqlx::Schema db = (*connection).getSchema(dataBaseName);
+    mysqlx::Table dbTable = db.getTable(table);
+
+    std::vector<std::string> fields;
+    std::vector<std::string> values;
+
+    for (auto field = object.begin(); field != object.end(); ++field)
+    {
+        fields.push_back(field->first);
+        values.push_back(field->second);
+    }
+
+    (*connection).startTransaction();
+    try
+    {
+        dbTable.insert(fields.begin(), fields.end()).values(values.begin(), values.end()).execute();
+        (*connection).commit();
+        return true;
+    }
+    catch (const mysqlx::Error &err)
+    {
+        (*connection).rollback();
+    }
+
+    return false;
 }
 
-void ORM::Update(std::string table, std::string objеct)
+bool ORM::Update(std::string &table, std::map<std::string, std::string> &object)
 {
-    if (!ConnectionDB(TEST_CONNECTION))
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
         throw Error("Connection is failed.");
 
-    std::ofstream SQLRequest(DIRECTORY_PATH, std::ofstream::out | std::ofstream::trunc);
-    SQLRequest << ("UPDATE " + table + "\nSET " + objеct + ";");
-    SQLRequest.close();
+    mysqlx::Schema db = (*connection).getSchema(dataBaseName);
+    mysqlx::Table dbTable = db.getTable(table);
+
+    std::string ID = object["ID"];
+
+    std::vector<std::string> fields;
+    std::vector<std::string> values;
+
+    for (auto field = object.begin(); field != object.end(); ++field)
+    {
+        if (field->first != "ID")
+        {
+            fields.push_back(field->first);
+            values.push_back(field->second);
+        }
+    }
+
+    (*connection).startTransaction();
+    try
+    {
+        mysqlx::TableUpdate updateTable = dbTable.update();
+        for (int i = 0; i < object.size(); ++i)
+            updateTable.set(fields[i], values[i]);
+
+        updateTable.where("ID = " + ID).execute();
+
+        (*connection).commit();
+        return true;
+    }
+    catch (const mysqlx::Error &err)
+    {
+        (*connection).rollback();
+    }
+
+    return false;
 }
 
-bool ORM::ConnectionDB(std::string connection) // очень грубая заглушка
+bool ORM::CreateTable(std::string &table, std::vector<std::string> &columns_names_and_types)
 {
-    return connection == "connectionMOCK";
+
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
+        throw Error("Connection is failed.");
+
+    (*connection).startTransaction();
+    try
+    {
+        std::string SQLRequest = "CREATE TABLE " + table;
+        for (std::string column : columns_names_and_types)
+            SQLRequest += " " + column;
+        (*connection).sql(SQLRequest);
+        (*connection).commit();
+        return true;
+    }
+    catch (const mysqlx::Error &err)
+    {
+        (*connection).rollback();
+    }
+
+    return false;
+}
+
+bool ORM::DeleteTable(std::string &table)
+{
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
+        throw Error("Connection is failed.");
+
+    (*connection).startTransaction();
+    try
+    {
+        (*connection).sql("DROP TABLE " + table);
+        (*connection).commit();
+        return true;
+    }
+    catch (const mysqlx::Error &err)
+    {
+        (*connection).rollback();
+    }
+
+    return false;
+}
+
+bool ORM::DropDatabase(std::string &database)
+{
+    std::unique_ptr<mysqlx::Session> connection = ConnectionDB();
+    if (!connection)
+        throw Error("Connection is failed.");
+
+    try
+    {
+        (*connection).dropSchema(dataBaseName);
+        return true;
+    }
+    catch (const Error &err)
+    {
+        std::cerr << "Error: " << err.what() << std::endl;
+    }
+
+    return false;
+}
+
+std::unique_ptr<mysqlx::Session> ORM::ConnectionDB()
+{
+    std::ifstream SQLConfig(pathToConfigFile);
+    std::string Host;
+    std::getline(SQLConfig, Host);
+    boost::algorithm::trim(Host = Host.substr(Host.find(":") + 1));
+    std::string Port;
+    std::getline(SQLConfig, Port);
+    boost::algorithm::trim(Port = Port.substr(Port.find(":") + 1));
+    std::string Username;
+    std::getline(SQLConfig, Username);
+    boost::algorithm::trim(Username = Username.substr(Username.find(":") + 1));
+    std::string Password;
+    std::getline(SQLConfig, Password);
+    boost::algorithm::trim(Password = Password.substr(Password.find(":") + 1));
+    SQLConfig.close();
+
+    try
+    {
+        auto session = std::make_unique<mysqlx::Session>(Host, std::stoi(Port), Username, Password);
+        return session;
+    }
+    catch (const mysqlx::Error &err)
+    {
+        return nullptr;
+    }
+    return nullptr;
 }
