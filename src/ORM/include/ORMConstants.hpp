4f714862a6eeb311b2dc525ffb20eecfb879e71f
Vladislav Fomenko <vladislav.fomenko.2014@gmail.com>
2023-06-01 00:17:49 +0300

    Final ORM

--- src/ORM/include/ORMConstants.hpp
+++ src/ORM/include/ORMConstants.hpp
@@ -2,9 +2,7 @@
 #include <map>
 
 const std::string MODELS = "../models.json";
-const std::string SCHEMA = "../schema.json";
 const std::string CONFIG = "../config.json";
-
 
 const std::map<std::string, std::string> convertTypes = {
     {"integer", "INT"},
