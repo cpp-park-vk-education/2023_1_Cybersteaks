361eceaaaa29d2d55cfdc178c2d6bb4fee9374f4
wonderf00l <maks22036@gmail.com>
2023-06-01 01:12:05 +0300

    добавил опцию выбора кластера/группы внутир кластера с учетом get параметров, пример в quiery.sh

--- backend/controllers/Clusters.cc
+++ backend/controllers/Clusters.cc
@@ -2,11 +2,30 @@
 #include <vector>
 #include <string>
 
-void Clusters::main(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+void Clusters::main(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, const std::string &type) {
     LOG_DEBUG << "someone is trying to get clusters list";
 
     Json::Value json;
-    std::vector<std::string> clusters = {"Musicians", "Artists", "Programmers"}; // from db
+
+    std::vector<std::string> clusters;
+    bool contains = false;
+
+    if (type.find("musicians") != std::string::npos) {
+        clusters.push_back("Musicians");
+        contains = true;
+    }
+    if (type.find("artists") != std::string::npos) {
+        clusters.push_back("Artists");
+        contains = true;
+    }
+    if (type.find("programmers") != std::string::npos) {
+        clusters.push_back("Programmers");
+        contains = true;
+    }
+
+    if (!contains) {
+        clusters = {"Musicians", "Artists", "Programmers"};
+    }
 
     json["clusters"] = Json::arrayValue;
 
@@ -25,11 +44,21 @@ void Clusters::main(const HttpRequestPtr &req, std::function<void(const HttpResp
 
 }
 
-void Clusters::groups(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+void Clusters::groups(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, const std::string &type) {
     LOG_DEBUG << "someone is trying to get clusters groups list";
 
+    std::vector<std::string> groups;
+
     Json::Value json;
-    std::vector<std::string> groups = {"NoPainNoGain", "MusiciansClusterBastards", "HardRockPayesOff"};
+    if (type == "musicians") {
+        groups = {"NoPainNoGain", "MusiciansClusterBastards", "HardRockPayesOff"};
+    }
+    if (type == "artists") {
+        groups = {"LoveArt", "ArtOrDart", "ArtistsForGood"};
+    }
+    if (type == "programmers") {
+        groups = {"BornToCoDE", "CodersForModers", "TechnoparkBMSTUGROUP"};
+    }
 
     json["cluster_groups"] = Json::arrayValue;
 
